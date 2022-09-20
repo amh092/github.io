19d65ea674faeeaaa657cfd0e34d0afa519929ef
amh092 <ahmedalhazza@Ahmeds-MacBook-Pro.local>
2022-09-21 02:36:22 +0300

    dfa

--- animation.js
+++ animation.js
@@ -1,43 +1,42 @@
 //when page load do
 
 const gallery = document.getElementById("works");
-const works = document.querySelector(".works").children;
+const works = document.querySelector(".works");
 
-const contact = document.getElementById('social')
-const social = document.querySelector('.contacts').children
-// scroll on gallery section 
+const contact = document.getElementById("social");
+const social = document.querySelector(".contacts").children
+// scroll on gallery section
 window.onscroll = function () {
-  if (gallery.getBoundingClientRect().top < 800)
-    for (let index = 0; index < works.length; index++) {
-      works[index].classList.add(`left-0`);
-      works[index].classList.remove(`rotate-45`);
+  function animationStart(index) {
+    setTimeout(() => {
+      works.children[index].classList.add(`left-0`);
+      works.children[index].classList.remove(`rotate-45`);
       setTimeout(() => {
-        works[index].classList.remove(`relative`);
-      }, 1000);
-    }
-  else if (gallery.getBoundingClientRect().top > 900)
-    for (let index = 0; index < works.length; index++) {
-      works[index].classList.remove(`left-0`);
-      works[index].classList.add(`left-40`);
-      works[index].classList.add(`rotate-45`);
-      works[index].classList.add(`relative`);
-    }
-   if (contact.getBoundingClientRect().top < 790 || window.innerWidth > 500){
-    for (let index = 0; index < social.length; index++) {
-        social[index].classList.add(`opacity-100`)
-        social[index].classList.remove(`opacity-0`)
-      
-      }}
-   else if (contact.getBoundingClientRect().top > 700 || window.innerWidth > 500){
-    for (let index = 0; index < social.length; index++) {
-        social[index].classList.add(`opacity-0`)
-        social[index].classList.remove(`opacity-100`)
-
+        works.children[index].classList.remove(`relative`);
+      }, 700);
+    }, 400 * index);
+  }
+  
+  function animationStartForContacts(index) {
+    setTimeout(() => {
        
-      }
-};
+                 social[index].classList.add(`opacity-100`)
+                    social[index].classList.remove(`opacity-0`)
+      }, 300*index);
+    }
 
 
+
+  for (let index = 0; index < works.children.length; index++) {
+    animationStart(index);
+  }
+  for (let index = 0; index < social.length; index++) {
+    animationStartForContacts(index)}
+
+  if (gallery.getBoundingClientRect().top < 800) {
+    animationStart();
+  }
+  if (contact.getBoundingClientRect().top < 790 || window.innerWidth > 500){
+    animationStartForContacts()
+  }
 }
-
-
