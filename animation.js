631586338cc97f0e315fc5416f94a005f401c2b4
amh092 <ahmedalhazza@Ahmeds-MacBook-Pro.local>
2022-09-21 03:10:16 +0300

    d

--- animation.js
+++ animation.js
@@ -1,12 +1,33 @@
 //when page load do
-
+const body = document.getElementById('body')
+const wait = document.getElementById('wait')
 const gallery = document.getElementById("works");
 const works = document.querySelector(".works");
 
 const contact = document.getElementById("social");
-const social = document.querySelector(".contacts").children
+const social = document.querySelector(".contacts")
+
+
+window.onload = 
+function functionName(){
+ 
+        
+        body.classList.add('opacity-100')
+
+}
 // scroll on gallery section
 window.onscroll = function () {
+    if (gallery.getBoundingClientRect().top < 800) {
+        for (let index = 0; index < works.children.length; index++) {
+          animationStart(index);
+        }
+      }
+        if (contact.getBoundingClientRect().top  < 750 || window.innerWidth > 500){
+        for (let index = 0; index < social.children.length; index++) {
+          animationStartForContacts(index)}
+        }
+      
+
   function animationStart(index) {
     setTimeout(() => {
       works.children[index].classList.add(`left-0`);
@@ -14,29 +35,23 @@ window.onscroll = function () {
       setTimeout(() => {
         works.children[index].classList.remove(`relative`);
       }, 700);
-    }, 400 * index);
+    }, 90 * index);
   }
   
   function animationStartForContacts(index) {
     setTimeout(() => {
        
-                 social[index].classList.add(`opacity-100`)
-                    social[index].classList.remove(`opacity-0`)
-      }, 300*index);
+                 social.children[index].classList.add(`opacity-100`)
+                    social.children[index].classList.remove(`opacity-0`)
+      }, 100*index);
     }
 
 
-
-  for (let index = 0; index < works.children.length; index++) {
-    animationStart(index);
-  }
-  for (let index = 0; index < social.length; index++) {
-    animationStartForContacts(index)}
-
-  if (gallery.getBoundingClientRect().top < 800) {
+ 
     animationStart();
-  }
-  if (contact.getBoundingClientRect().top < 790 || window.innerWidth > 500){
+  
+ 
     animationStartForContacts()
-  }
+  
+
 }
