6c201d4cb14cf8d7227d63011067db5cb84a7481
ahmed mohammed <65685434+amh092@users.noreply.github.com>
2022-09-25 01:18:48 +0300

    Update animation.js

--- animation.js
+++ animation.js
@@ -1,7 +1,10 @@
 //when page load do
+window.location.href = 'https://amh092.github.io/portofolio/'
 
 const gallery1 = document.getElementById("works");
 const works = document.querySelector(".works");
+const header = document.getElementById('header');
+const me = document.getElementById('me');
 
 const contactMe = document.getElementById("social");
 const social = document.querySelector(".contacts")
@@ -16,7 +19,6 @@ function animationStart(index) {
     }, 200);
   }, 300 * index);
 }
-
 function animationStartForContacts(index) {
   setTimeout(() => {
      
@@ -24,29 +26,43 @@ function animationStartForContacts(index) {
                   social.children[index].classList.remove(`opacity-0`)
     }, 100*index);
   }
+  function headerBlack(){
+    header.classList.remove('bg-transparent')
+    header.classList.add('bg-black')
+    header.classList.add('fixed')
 
-
-
+  
+  }
+  function headerTransparent(){
+    header.classList.remove('bg-black');
+    header.classList.remove('fixed')
+    header.classList.add('bg-transparent')
+  }
 // scroll on gallery section
 window.onscroll = function () {
     if (gallery1.getBoundingClientRect().top < 800) {
         for (let index = 0; index < works.children.length; index++) {
           animationStart(index);
         }
+       
       }
         if (contactMe.getBoundingClientRect().top  < 750 || window.innerWidth > 500){
+
         for (let index = 0; index < social.children.length; index++) {
-          animationStartForContacts(index)}
+        
+        }
          
         }
-    
 
-
- 
-   
-  
-
+        if(me.getBoundingClientRect().top <0){
+         headerBlack()
+         console.log(me.getBoundingClientRect().top)
+        }
+        else{
+          headerTransparent()
+        }
+       
+     
+       
+        
 }
-
-  
-  
